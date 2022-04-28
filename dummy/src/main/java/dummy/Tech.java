1a183c0bb919463b022f0d749b19f387c68c6094
elavarasan.p <elavarasan.p@DT150150.QICHOAPPL.com>
2022-04-28 10:49:16 +0530

    sdfgsd

--- dummy/src/main/java/dummy/Tech.java
+++ dummy/src/main/java/dummy/Tech.java
@@ -3,7 +3,7 @@ package dummy;
 public class Tech {
 
 	public static void main(String[] args) {
-		System.out.println("Elagdf");
+		System.out.println("Elagxdf");
 		// TODO Auto-generated method stub
 
 	}
