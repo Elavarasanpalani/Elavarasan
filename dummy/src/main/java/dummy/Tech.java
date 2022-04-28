0efe9285439440d279784b046094f49c7f91347d
elavarasan.p <elavarasan.p@DT150150.QICHOAPPL.com>
2022-04-28 11:05:11 +0530

    Merge branch 'master' of https://github.com/Elavarasanpalani/Elavarasan.git s

--- dummy/src/main/java/dummy/Tech.java
+++ dummy/src/main/java/dummy/Tech.java
@@ -3,7 +3,7 @@ package dummy;
 public class Tech {
 
 	public static void main(String[] args) {
-		System.out.println("Elagdxdf");
+		System.out.println("Elagdxdcf");
 		// TODO Auto-generated method stub
 
 	}
