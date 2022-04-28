24962d5c3aafdfef4873b86311799e1ca8110eee
elavarasan.p <elavarasan.p@DT150150.QICHOAPPL.com>
2022-04-28 10:41:51 +0530

    dfds

--- Short/src/main/java/Simple.java
+++ Short/src/main/java/Simple.java
@@ -2,7 +2,7 @@
 public class Simple {
 
 	public static void main(String[] args) {
-		System.out.println("ekrfrrga");
+		System.out.println("ekrfrrdfga");
 		// TODO Auto-generated method stub
 
 	}
