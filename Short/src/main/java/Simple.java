41d03496e7079c2ab517449745b15be9eb744caa
Elavarasanpalani <104414532+Elavarasanpalani@users.noreply.github.com>
2022-04-29 10:05:31 +0530

    Update Simple.java

--- Short/src/main/java/Simple.java
+++ Short/src/main/java/Simple.java
@@ -2,7 +2,7 @@
 public class Simple {
 
 	public static void main(String[] args) {
-		System.out.println("ekrfrrdfga");
+		System.out.println("ekdrfrrdfga");
 		// TODO Auto-generated method stub
 
 	}
