ed47ad438c85946ad9e14fc25d426b1e3331156f
Elavarasanpalani <104414532+Elavarasanpalani@users.noreply.github.com>
2022-05-04 11:33:44 +0530

    Update Opera.java

--- kali/src/main/java/kali/Opera.java
+++ kali/src/main/java/kali/Opera.java
@@ -3,7 +3,7 @@ package kali;
 public class Opera {
 
 	public static void main(String[] args) {
-		System.out.println("helloworld");
+		System.out.println("hello muthu world");
 		// TODO Auto-generated method stub
 
 	}
