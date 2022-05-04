74fca035b476c724a77f35f930808cd336651b70
elavarasan.p <elavarasan.p@DT150150.QICHOAPPL.com>
2022-05-04 14:17:23 +0530

    dfadf

---
+++ xman/src/main/java/xman/Tree.java
@@ -0,0 +1,11 @@
+package xman;
+
+public class Tree {
+
+	public static void main(String[] args) {
+		System.out.println("This my new project of elavarasan");
+		// TODO Auto-generated method stub
+
+	}
+
+}
